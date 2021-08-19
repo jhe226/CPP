13d1c66ad146b26b7cf951a0e57222a0ef40bfe1
jhe226 <79580469+jhe226@users.noreply.github.com>
2021-08-19 13:39:21 +0900

    Add files via upload

---
+++ 210819/06_ref_return_02/06_ref_return_02.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+// 반환형이 참조이고 반환도 참조로 받는 경우
+
+int &RefRetFuncOne(int &ref)
+{
+	// 참조형으로 반환
+	ref++;
+	return ref;
+}
+
+int main(void)
+{
+	int num1 = 1;
+	int &num2 = RefRetFuncOne(num1);	// 반환도 참조로 받음
+
+	num1 += 1;
+	num2 += 100;
+	cout << "num1 : " << num1 << endl;
+	cout << "num2 : " << num2 << endl;
+
+	cout << "&num1 : " << &num1 << endl;
+	cout << "&num2 : " << &num2 << endl;
+
+	return 0;
+
+}
